71e360e71d5fc3d9d0f2c72c970421892b6b7eed
Chuck Hinson <190260+chuckhinson@users.noreply.github.com>
2023-03-28 23:07:11 -0400

    update mkcluster to put workers in private subnet instead of public

--- mkcluster.sh
+++ mkcluster.sh
@@ -9,6 +9,8 @@ PRIVATE_SUBNET2=$(terraform -chdir=./terraform output -json | jq -r '.private_su
 PUBLIC_SUBNET1=$(terraform -chdir=./terraform output -json | jq -r '.public_subnet1.value')
 PUBLIC_SUBNET2=$(terraform -chdir=./terraform output -json | jq -r '.public_subnet2.value')
 
-# NOTE: if you want to do dry-run, you cant use --profile, so you'll need to set AWS_PROFILE env variable
+# NOTE: if you want to do dry-run, you cant use --profile, so you willl need to set AWS_PROFILE env variable
+
 eksctl create cluster  --profile ${AWS_PROFILE_NAME} --name ${PROJECT_NAME} --region us-gov-west-1  \
-        --vpc-public-subnets "${PUBLIC_SUBNET1},${PUBLIC_SUBNET2}" --vpc-private-subnets "${PRIVATE_SUBNET1},${PRIVATE_SUBNET2}"
+        --vpc-public-subnets "${PUBLIC_SUBNET1},${PUBLIC_SUBNET2}" --vpc-private-subnets "${PRIVATE_SUBNET1},${PRIVATE_SUBNET2}" \
+        --node-private-networking
